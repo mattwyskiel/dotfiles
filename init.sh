96b6752a5160ed682d74902c509af423128a5549
Matthew Wyskiel <matt@mattwyskiel.com>
2025-04-14 18:59:00 -0400

    Initial Commit (Alacritty, Git, ZSH)

---
+++ init.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# This script is meant set up a fresh computer.
+# Goal is for this to work on macOS, Linux, and WSL.
+
+# Check if homebrew is installed
+# If not, install it
+if ! command -v brew &> /dev/null; then
+    echo "Homebrew not found. Installing..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed."
+fi
+
+# Check if GNU Stow is installed
+# If not, install it
+if ! command -v stow &> /dev/null; then
+    echo "GNU Stow not found. Installing..."
+    brew install stow
+else
+    echo "GNU Stow is already installed."
+fi
+
+# Check if Alacritty is installed
+# If not, install it
+if ! [ -d "/Applications/Alacritty.app" ]; then
+    echo "Alacritty not found. Installing..."
+    brew install alacritty
+else
+    echo "Alacritty is already installed."
+
+fi
+
+# Check if Hack Nerd Font is installed
+# If not, install it
+if ! [ -f "$HOME/Library/Fonts/HackNerdFont-Regular.ttf" ]; then
+    echo "Hack Nerd Font not found. Installing..."
+    brew install font-hack-nerd-font
+else
+    echo "Hack Nerd Font is already installed."
+fi
+
+echo "Symlinking alacritty config..."
+stow --dir="$HOME/dotfiles" --target="$HOME" alacritty
+echo "...done."
+
+# Install oh-my-posh
+if ! command -v oh-my-posh &> /dev/null; then
+    echo "Oh My Posh not found. Installing..."
+    brew install jandedobbeleer/oh-my-posh/oh-my-posh
+else
+    echo "Oh My Posh is already installed."
+fi
+
+# Install tmux
+if ! command -v tmux &> /dev/null; then
+    echo "Tmux not found. Installing..."
+    brew install tmux
+else
+    echo "Tmux is already installed."
+fi
+
+echo "Symlinking zsh config..."
+stow --dir="$HOME/dotfiles" --target="$HOME" zsh
+echo "...done."
+
+echo "Symlinking git config..."
+stow --dir="$HOME/dotfiles" --target="$HOME" git
+echo "...done."
+
---
+++ zsh/.zshrc
@@ -0,0 +1,30 @@
+# pnpm
+export PNPM_HOME="/Users/matthewwyskiel/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# sst
+export PATH=/Users/matthewwyskiel/.sst/bin:$PATH
+
+# Created by `pipx` on 2024-04-25 17:37:44
+export PATH="$PATH:/Users/matthewwyskiel/.local/bin"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/matthewwyskiel/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/matthewwyskiel/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/matthewwyskiel/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/matthewwyskiel/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
+# The following lines have been added by Docker Desktop to enable Docker CLI completions.
+fpath=(/Users/matthewwyskiel/.docker/completions $fpath)
+autoload -Uz compinit
+compinit
+# End of Docker CLI completions
+
+eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/avit.omp.json)"
+
