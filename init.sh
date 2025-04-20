1c0a614096cb1809b195674ac8eeaaf1370a508e
Matthew Wyskiel <matt@mattwyskiel.com>
2025-04-20 11:39:29 -0400

    add WSL support

--- init.sh
+++ init.sh
@@ -6,7 +6,8 @@ if [ "$(uname)" = "Darwin" ]; then
     echo "Detected macOS..."
     $HOME/dotfiles/scripts/init-macos.sh
 elif grep -qi microsoft /proc/version 2>/dev/null; then
-    echo "Detected WSL..."
+    echo "Detected WSL...using ubuntu init script"
+    $HOME/dotfiles/scripts/init-ubuntu.sh
 elif [ -f /etc/os-release ] && grep -qi ubuntu /etc/os-release; then
     echo "Detected Ubuntu..."
     $HOME/dotfiles/scripts/init-ubuntu.sh
