307b1a45117260be60f3112cbacd0fdf58d1c96d
Matthew Wyskiel <matt@mattwyskiel.com>
2025-05-01 07:39:30 -0400

    [nvim] set tabs to 4 spaces

--- nvim/.config/nvim/lua/mattwyskiel/opts.lua
+++ nvim/.config/nvim/lua/mattwyskiel/opts.lua
@@ -18,6 +18,11 @@ vim.opt.number = true
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
