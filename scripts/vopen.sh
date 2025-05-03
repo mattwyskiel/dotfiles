10c04ca17a97cea7afe638c43512b425cf4827e3
Matthew Wyskiel <matt@mattwyskiel.com>
2025-05-03 10:30:06 -0400

    [scripts] fix vopen

--- scripts/vopen.sh
+++ scripts/vopen.sh
@@ -6,7 +6,13 @@
 DIR="${1:-$HOME}"
 # Get the desired session name, if not provided, default to the current directory name
 SESSION_NAME="${2:-$(basename "$DIR")}"
-
+# Check if the session already exists
+# If it does, attach to it
+if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+    echo "Session $SESSION_NAME already exists. Attaching..."
+    tmux attach-session -t "$SESSION_NAME"
+    exit 0
+fi
 tmux new-session -d -c "$DIR" -s "$SESSION_NAME" # start detached session in DIR
 # Split horizontally (-v) so the new pane is below, focus goes to it
 tmux split-window -v -c "$DIR"
