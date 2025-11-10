e34a44927b10b3708f56c0f30e4c8bd3964de1a0
Matthew Wyskiel <matt@mattwyskiel.com>
2025-11-10 14:45:16 -0800

    fix(vscode): turn off most neovim things for VSCode integration support

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,38 +1,42 @@
-require 'mattwyskiel.opts' -- general options
+if not vim.g.vscode then
+  require 'mattwyskiel.opts' -- general options
 
-require 'mattwyskiel.keymaps' -- keymaps
-require 'mattwyskiel.autocommands' -- autocommands
-require 'mattwyskiel.filetypes' -- filetypes
+  require 'mattwyskiel.keymaps' -- keymaps
+  require 'mattwyskiel.autocommands' -- autocommands
+  require 'mattwyskiel.filetypes' -- filetypes
 
-require 'mattwyskiel.lazy' -- install lazy.nvim
+  require 'mattwyskiel.lazy' -- install lazy.nvim
 
-require('lazy').setup({
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-  require 'mattwyskiel.plugins.gitsigns', -- Git signs in the gutter
-  require 'mattwyskiel.plugins.which-key', -- Keymaps in a popup
-  require 'mattwyskiel.plugins.neo-tree', -- File explorer
-  require 'mattwyskiel.plugins.telescope', -- Fuzzy finder
-  require 'mattwyskiel.plugins.telescope-file-browser', -- File browser for telescope
-  require 'mattwyskiel.plugins.autopairs', -- Autopairs for brackets, quotes, etc.
+  require('lazy').setup({
+    'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+    require 'mattwyskiel.plugins.gitsigns', -- Git signs in the gutter
+    require 'mattwyskiel.plugins.which-key', -- Keymaps in a popup
+    require 'mattwyskiel.plugins.neo-tree', -- File explorer
+    require 'mattwyskiel.plugins.telescope', -- Fuzzy finder
+    require 'mattwyskiel.plugins.telescope-file-browser', -- File browser for telescope
+    require 'mattwyskiel.plugins.autopairs', -- Autopairs for brackets, quotes, etc.
 
-  -- require 'mattwyskiel.plugins.indent_line', -- Indent line for showing indentation levels
-  require 'mattwyskiel.plugins.lazydev', -- LazyDev for lazy loading plugins
-  require 'mattwyskiel.plugins.lspconfig', -- LSP configuration
-  'b0o/schemastore.nvim', -- JSON schema store
-  require 'mattwyskiel.plugins.conform', -- LSP formatter
-  require 'mattwyskiel.plugins.lint', -- Linting
-  require 'mattwyskiel.plugins.blink-cmp', -- Completion engine
+    -- require 'mattwyskiel.plugins.indent_line', -- Indent line for showing indentation levels
+    require 'mattwyskiel.plugins.lazydev', -- LazyDev for lazy loading plugins
+    require 'mattwyskiel.plugins.lspconfig', -- LSP configuration
+    'b0o/schemastore.nvim', -- JSON schema store
+    require 'mattwyskiel.plugins.conform', -- LSP formatter
+    require 'mattwyskiel.plugins.lint', -- Linting
+    require 'mattwyskiel.plugins.blink-cmp', -- Completion engine
 
-  require 'mattwyskiel.plugins.tokyonight', -- Colorscheme
-  require 'mattwyskiel.plugins.todo-comments', -- Highlight todo, notes, etc in comments
-  require 'mattwyskiel.plugins.mini', -- Mini.nvim for various small plugins
-  require 'mattwyskiel.plugins.treesitter', -- Treesitter for syntax highlighting
+    require 'mattwyskiel.plugins.tokyonight', -- Colorscheme
+    require 'mattwyskiel.plugins.todo-comments', -- Highlight todo, notes, etc in comments
+    require 'mattwyskiel.plugins.mini', -- Mini.nvim for various small plugins
+    require 'mattwyskiel.plugins.treesitter', -- Treesitter for syntax highlighting
 
-  require 'mattwyskiel.plugins.markdown',
-  require 'mattwyskiel.plugins.copilot', -- GitHub Copilot
-}, {
-  ui = require 'mattwyskiel.lazy.ui', -- UI for lazy.nvim
-})
-
+    require 'mattwyskiel.plugins.markdown',
+    require 'mattwyskiel.plugins.copilot', -- GitHub Copilot
+    cond = function()
+      return not vim.g.vscode
+    end,
+  }, {
+    ui = require 'mattwyskiel.lazy.ui', -- UI for lazy.nvim
+  })
+end
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
