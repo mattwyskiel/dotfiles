0636901b4b8a4efa76cfc4ba1c299033f7a35b7e
Matthew Wyskiel <matt@mattwyskiel.com>
2026-08-14 16:02:53 -0400

    feat(nvim): support local .nvim.lua files

--- nvim/.config/nvim/lua/mattwyskiel/opts.lua
+++ nvim/.config/nvim/lua/mattwyskiel/opts.lua
@@ -12,6 +12,9 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Load trusted project-local .nvim.lua files.
+vim.o.exrc = true
+
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
